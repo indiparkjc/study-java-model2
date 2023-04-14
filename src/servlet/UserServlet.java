72bf34607a49e04409ca09c7e6ae0d86f361178d
indiparkjc <indiparkjc@indisystem.co.kr>
2023-04-14 15:35:09 +0900

    Servlet Sample

---
+++ src/servlet/UserServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("[요청] UserServlet 객체 생성");
+    }
+
+//    @Override
+//    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+//        super.service(req, res);
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("GET 호출!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("POST 호출!");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("[종료] UserServlet 객체 제거");
+    }
+}
